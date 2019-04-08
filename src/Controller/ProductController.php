77f8a4aa96bbbca7311970b4ac55c20e01cb6da6
Laugras <lew_terramp@hotmail.fr>
2019-04-08 09:46:34 +0200

    Reprise du projet en cours

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use App\Form\ProductType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/product/create", name="product_create")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function create(Request $request)
+    {
+        $repository = $this->getDoctrine()->getRepository(Product::class);
+        $products = $repository->findAll();
+
+        dump($products);
+
+        $product = new Product();
+        $form = $this->createForm(ProductType::class, $product);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Ajouter le produit en BDD
+            $manager = $this->getDoctrine()->getManager();
+
+            $manager->persist($product);
+            $manager->flush();
+        }
+
+        return $this->render('product/create.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
