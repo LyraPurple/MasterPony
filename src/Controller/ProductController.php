1eb8299130b707c5953a4289a075a2f4a363f861
Laugras <lew_terramp@hotmail.fr>
2019-04-08 17:02:29 +0200

    Sans faker

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -6,6 +6,7 @@ use App\Entity\Product;
 use App\Form\ProductType;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -50,6 +51,7 @@ class ProductController extends AbstractController
                 'Le produit '.$id.' n\'existe pas'
             );
         }
+
         return $this->render('product/show.html.twig', [
             'product' => $product
         ]);
@@ -70,5 +72,53 @@ class ProductController extends AbstractController
         return $this->render('product/list.html.twig', [
            'products' => $products
         ]);
+    }
+
+    /**
+     * @Route("/product/edit/{id}", name="product_edit")
+     * @param Request $request
+     * @param Product $product
+     * @return Response
+     */
+    public function edit(Request $request, Product $product)
+    {
+        $form = $this->createForm(ProductType::class, $product);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Le persist est optionnel
+            $this->getDoctrine()->getManager()->flush();
+            $this->addFlash('success', 'Le produit '.$product->getId().' a bien été modifié.');
+
+            return $this->redirectToRoute('product_list');
+        }
+
+        return $this->render('product/edit.html.twig', [
+            'product' => $product,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/product/delete/{id}", name="product_delete", methods={"POST"})
+     * @param Request $request
+     * @param Product $product
+     * @return RedirectResponse
+     */
+    public function delete(Request $request, Product $product)
+    {
+        if (!$this->isCsrfTokenValid('delete', $request->get('token'))) {
+            return $this->redirectToRoute('product_list');
+        }
+
+        // $em = $entityManager
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($product);
+        $em->flush();
+
+        $this->addFlash('success', 'Le produit '.$product->getName().' a bien été supprimé');
+
+        return $this->redirectToRoute('product_list');
     }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -1,25 +1,17 @@
 <?php
-
 namespace App\DataFixtures;
-
 use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
-use Faker;
-
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // Tutoriel: https://blog.dev-web.io/2018/01/20/symfony-4-creation-de-fixtures-aleatoires-faker/
-        // On configure dans quelles langues nous voulons nos données
-        $faker = Faker\Factory::create('fr_FR');
-        // on créé 100 cartes
-        for ($i = 1; $i < 101; $i++) {
+        for ($i = 1; $i <= 100; $i++) {
             $product = new Product();
-            $product->setName($faker->unique()->numerify('iPhone ##'));
-            $product->setDescription($faker->text);
-            $product->setPrice($faker->numberBetween($min = 100, $max = 100000));
+            $product->setName('iPhone '.$i);
+            $product->setDescription('Un iPhone de '.$i);
+            $product->setPrice(rand(10, 10000) * 100);
             $manager->persist($product);
         }
         $manager->flush();
